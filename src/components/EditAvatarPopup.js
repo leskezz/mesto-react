78077414a1b9b9daeee727bfe41b0dcd95f6c911
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2020-10-03 21:28:44 +1000

    fixed: Удали лишние сущности в EditAvatarPopup

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm.js';
-import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
 function EditAvatarPopup(props) {
 
